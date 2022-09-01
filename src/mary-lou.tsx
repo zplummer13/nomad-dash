2be4cf43ff6f6cf372b3ca75762461b2ec4d5a88
zplummer13 <zplummer13@gmail.com>
2022-09-01 09:04:16 -0400

    september update

--- src/mary-lou.tsx
+++ src/mary-lou.tsx
@@ -10,13 +10,13 @@ function MaryLou(props: OwnProps) {
     const currentCoffeeFlavor = getCurrentCoffeeFlavor();
 
     return (
-        <div className="flex flex-col items-center p-1 drop-shadow-lg bg-zblue rounded-md font-DM-Serif font-semibold text-2xl text-ztan">
+        <div className="flex flex-col items-center p-1 drop-shadow-lg bg-zblue rounded-md font-Sriracha text-2xl text-ztan">
             <div className="flex gap-3 text-3xl">
                 <span className="text-pink-400">
                     <BiCoffeeTogo />
                 </span>
                 <span>
-                    Marylou's FotM
+                    Marylou's Coffee
                 </span>
 
             </div>
